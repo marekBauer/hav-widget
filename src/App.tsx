e9461e2867b76260757e510a720dba6b3eecd441
marekBauer <marek.bauer@atreo.cz>
2024-11-07 09:02:22 +0100

    warning

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ function App() {
       <form className="formm">
         <label>
           Phone:
-          <input type="text" name="phone" value="123456789" />
+          <input type="text" name="phone" defaultValue="123456789" />
         </label>
         <label>
           Email:
-          <input type="email" name="email" value="test@email.cz" />
+          <input type="email" name="email" defaultValue="test@email.cz" />
         </label>
 
         <div className="ageproof-cz" data-apikey={VERIFICATION_API_KEY}></div>
